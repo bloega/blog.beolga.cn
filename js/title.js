d7a30d816613e1753318f43995c32e5e8158700e
beolga <1586865771@qq.com>
2022-12-17 10:53:40 +0800

    Site updated: 2022-12-17 10:53:36

--- js/title.js
+++ js/title.js
@@ -1,1 +1,1 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="w(ﾟДﾟ)w 不要走！再看看嘛！",clearTimeout(titleTime)):(document.title="♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="404 not found",clearTimeout(titleTime)):(document.title="骗你的啦♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
