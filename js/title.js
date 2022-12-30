3a0b033cbfc0c2125fc2fba704bf3e46683953ae
beolga <1586965771@qq.com>
2022-12-30 09:59:04 +0800

    Site updated: 2022-12-30 09:58:54

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '404 not found';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '骗你的啦♪(^∇^*)欢迎肥来！' + OriginTitile;
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="404 not found",clearTimeout(titleTime)):(document.title="骗你的啦♪(^∇^*)欢迎肥来！"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
